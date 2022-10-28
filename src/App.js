7c9fb6f98efdb8caaafe0b2892e3bd7723a3130e
Farhan Farasdak <farhan.farasdak@flash-coffee.com>
2022-10-28 20:09:45 +0700

    add home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Register from './pages/register';
 import { Routes, Route } from "react-router-dom";
 import Login from './pages/login';
+import Home from './pages/home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/home" element={<Home/>} />
       </Routes>
     </div>
   );
