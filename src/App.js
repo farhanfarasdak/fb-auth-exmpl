3a89a83cb82a939d3a62a5069c763cc9989e3236
Farhan Farasdak <farhan.farasdak@flash-coffee.com>
2022-10-28 20:05:03 +0700

    Create login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Register from './pages/register';
 import { Routes, Route } from "react-router-dom";
+import Login from './pages/login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </div>
   );
