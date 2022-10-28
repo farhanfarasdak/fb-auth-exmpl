f731df69958659a3d63b0c68e21bd0254d9c5da3
Farhan Farasdak <farhan.farasdak@gmail.com>
2022-10-28 15:11:17 +0200

    add home page (#3)
    Co-authored-by: Farhan Farasdak <farhan.farasdak@flash-coffee.com>

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+const { Component } = require("react");
+
+class Home extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    )
+  }
+}
+
+export default Home
