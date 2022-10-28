8d3bd6f017e711a71a16737406c6c778a2e7bf4e
Farhan Farasdak <farhan.farasdak@flash-coffee.com>
2022-10-28 20:43:15 +0700

    log in using firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -10,3 +11,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const authFirebase = getAuth(app);
+
+export default authFirebase
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,13 +1,46 @@
+import authFirebase from "../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 const { Component } = require("react");
 
 class Register extends Component{
+  state={
+    email: '',
+    password: ''
+  }
+
+  handleRegister = () => {    
+    createUserWithEmailAndPassword(authFirebase, this.state.email, this.state.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        window.location.href = '/home'
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        alert(errorMessage)
+      });
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
   render(){
     return(
       <div>
         <h1>Register Page</h1>
-        <input placeholder="email"></input>
-        <input placeholder="password" type="password"></input>
-        <button type="submit">REGISTER</button>
+        <input 
+          placeholder="email" 
+          id="email"
+          onChange={this.handleOnChange}></input>
+        <input 
+          placeholder="password" 
+          type="password" 
+          id="password"
+          onChange={this.handleOnChange}></input>
+        <button type="submit" onClick={this.handleRegister}>REGISTER</button>
       </div>
     )
   }
