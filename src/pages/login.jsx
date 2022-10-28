cd4cd9d833af1eb164027535383c63f442b4442a
Farhan Farasdak <farhan.farasdak@flash-coffee.com>
2022-10-28 20:51:56 +0700

    Add logic firebase

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,47 @@
+import authFirebase from "../config/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
 const { Component } = require("react");
 
 class Login extends Component{
+  state={
+    email: '',
+    password: ''
+  }
+
+  handleLogin = () => {
+    signInWithEmailAndPassword(authFirebase, this.state.email, this.state.password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        window.location.href = '/home'
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        alert(errorMessage)
+      });
+
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
   render(){
     return(
       <div>
         <h1>Login Page</h1>
-        <input placeholder="email"></input>
-        <input placeholder="password" type="password"></input>
-        <button type="submit">LOGIN</button>
+        <input 
+          placeholder="email" 
+          id="email"
+          onChange={this.handleOnChange}></input>
+        <input 
+          placeholder="password" 
+          type="password" 
+          id="password"
+          onChange={this.handleOnChange}></input>
+        <button type="submit" onClick={this.handleLogin}>LOGIN</button>
       </div>
     )
   }
